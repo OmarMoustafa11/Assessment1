8e7bb1e73f1f2a82f0c8f8ca889ba02ef298c69e
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-13 15:28:34 -0400

    finished exam

--- src/main/java/firstAssessment.java
+++ src/main/java/firstAssessment.java
@@ -67,10 +67,11 @@ public class firstAssessment {
 
 
 
-
-   // public static void main(String[] args) {
-   // public static String extractAllDoubleDigitNumbers();{}
-       // int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int x = 0;
+        while (x >= 10);
+        x++;
+        System.out.println();
 
 
 
